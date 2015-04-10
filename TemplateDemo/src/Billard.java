bc5f9c881203c1afb9fbaa148359b8d52e6821c2
s11617 <st.sledziona@gmail.com>
2015-04-10 17:51:44 +0200

    first commit

---
+++ TemplateDemo/src/Billard.java
@@ -0,0 +1,23 @@
+
+public class Billard  extends Game{
+
+	@Override
+	void initialize() {
+		System.out.println("Billard Game Initialized!");
+		
+	}
+
+	@Override
+	void start() {
+		System.out.println("Billard Game Started");
+		
+	}
+
+	@Override
+	void end() {
+		System.out.println("Billard Game Ended");
+		
+	}
+
+	
+}
---
+++ TemplateDemo/src/TemplateTest.java
@@ -0,0 +1,11 @@
+
+public class TemplateTest {
+
+	public static void main(String[] args) {
+
+		Game game = new Billard();
+		game.play();
+
+	}
+
+}
